00d3c621a1097490ed3a6b6bda77f8e4f849b10b
R.A. Lucas <richard@richardalucas.com>
2014-02-02 09:14:58 -0700

    working on rest changeover

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var socketio = require('socket.io');
 var _ = require('underscore');
 var moment = require('moment');
 var range = require('moment-range');
+var calculate = require('./services/calculate');
 
 // Create server
 var server = http.createServer(app);
@@ -44,48 +45,6 @@ var mrdata = [];
 var distanceFrom = Number;
 var stops = [];
 
-//creates prototype to convert radii to distance
-Number.prototype.toRad = function() {
-  return this * Math.PI / 180;
-};
-
-//function for calculating distance
-function calculateDistance(obj, arr) {
-    var R = 3959; // m
-    var latitude = obj.lat;
-    var longitude = obj.lon;
-    var time = obj.time;
-    var gTime = moment().zone(420).format('HH:mm:ss');
-    console.log('gTime', gTime);
-    //adjusted for GMT to MST
-    var tenTime = moment().zone(420).add('m', 10).format('HH:mm:ss');
-    var timeRange = moment().range(time, tenTime);
-    var output = [];
-    _.each(arr, function(arrObj){
-        var dLat = (arrObj['stop_lat'] - latitude).toRad();
-        var dLon = (arrObj['stop_lon'] - longitude).toRad();
-        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(latitude.toRad()) * Math.cos(arrObj['stop_lat'].toRad()) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c;
-        arrObj['distance'] = Math.round(d*100)/100;
-        if((arrObj['distance'] <= obj['distanceFrom']) && (arrObj['arrival_time'] > gTime)  && (arrObj['arrival_time'] < tenTime)){
-            output.push(arrObj);
-        }
-    });
-    return output;
-}
-
-fs.readFile(__dirname + '/public/javascripts/mr_data.js',
-    function (err, data){
-        if(err){
-            console.log(err);
-        }
-        var d = JSON.parse(data);
-        mrdata = d;
-    });
-
 //socket events
 io.sockets.on('connection', function(socket){
 	console.log('sockets connected');
@@ -93,7 +52,7 @@ io.sockets.on('connection', function(socket){
 	//takes current location from client and runs
 	//it against stops and send back nearby stops
 	socket.on('currentLoc', function(data){
-		stops = calculateDistance(data, mrdata);
+		stops = calculate.calculateDistance(data, mrdata);
 		io.sockets.emit('stops', stops);
 	});
 
--- services/calculate.js
+++ services/calculate.js
@@ -1,41 +1,74 @@
+var fs = require('fs');
+var request = require('request');
+
 //creates prototype to convert radii to distance
 Number.prototype.toRad = function() {
   return this * Math.PI / 180;
 };
 
 //function for calculating distance
-function calculateDistance(obj, arr) {
+calculateDistance = function calculateDistance(currentPlaceObj, stops) {
     var R = 3959; // m
-    var latitude = obj.lat;
-    var longitude = obj.lon;
-    var time = obj.time;
+    var latitude = currentPlaceObj.lat;
+    var longitude = currentPlaceObj.lon;
+    var output = [];
+    _.each(stops, function(stop) {
+        var dLat = (stop['stop_lat'] - latitude).toRad();
+        var dLon = (stop['stop_lon'] - longitude).toRad();
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(latitude.toRad()) * Math.cos(stop['stop_lat'].toRad()) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        stop['distance'] = Math.round(d*100)/100;
+        if( stop['distance'] <= currentPlaceObj['distanceFrom'] ) {
+            output.push(stop);
+        }
+    });
+    return output;
+};
+
+// TODO: need function to match output of calculate distance to actual stop times
+
+var closestStops = calculateDistance(geolocation, stops); //geolocation comes from socket event
+
+exports.closestTimes = function (currentPlaceObj, stop_times) {
+    var output = [];
+    var time = currentPlaceObj.time;
     var gTime = moment().zone(420).format('HH:mm:ss');
     console.log('gTime', gTime);
     //adjusted for GMT to MST
     var tenTime = moment().zone(420).add('m', 10).format('HH:mm:ss');
     var timeRange = moment().range(time, tenTime);
-    var output = [];
-    _.each(arr, function(arrObj){
-        var dLat = (arrObj['stop_lat'] - latitude).toRad();
-        var dLon = (arrObj['stop_lon'] - longitude).toRad();
-        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(latitude.toRad()) * Math.cos(arrObj['stop_lat'].toRad()) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c;
-        arrObj['distance'] = Math.round(d*100)/100;
-        if((arrObj['distance'] <= obj['distanceFrom']) && (arrObj['arrival_time'] > gTime)  && (arrObj['arrival_time'] < tenTime)){
-            output.push(arrObj);
+
+    _.each(stop_times, function(stop_time) {
+        if(( stop_time['arrival_time'] > gTime )  && ( stop_time['arrival_time'] < tenTime )) {
+            output.push(stop_time);
         }
     });
     return output;
-}
+};
 
-fs.readFile(__dirname + '/public/javascripts/mr_data.js',
-    function (err, data){
-        if(err){
-            console.log(err);
-        }
-        var d = JSON.parse(data);
-        mrdata = d;
-    });
+
+// fs.readFile(__dirname + '/public/javascripts/mr_data.js',
+//     function (err, data){
+//         if(err){
+//             console.log(err);
+//         }
+//         var d = JSON.parse(data);
+//         mrdata = d;
+//     });
+
+var stop_times = request('http://busboy-api.herokuapp.com/api/stop_times', function(error, response, body) {
+    if( error ) { console.error('ERROR in stop_times request'); }
+    else if( response.statusCode === 200 ) {
+        return body;
+    }
+});
+
+var stops = request('http://busboy-api.herokuapp.com/api/stop_times', function(error, response, body) {
+    if( error ) { console.error('ERROR in stop_times request'); }
+    else if( response.statusCode === 200 ) {
+        return body;
+    }
+});
